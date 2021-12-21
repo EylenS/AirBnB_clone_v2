512b276807243dbcdc4075c759acc15fa891f637
Cristian Granada <crisgrvc@Cristians-MacBook-Pro.local>
2021-12-21 12:22:47 -0500

    update class City

--- models/city.py
+++ models/city.py
@@ -10,8 +10,8 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    # places = relationship(
-    #     "Place",
-    #     backref='cities',
-    #     cascade="all, delete",
-    #     passive_deletes=True)
+    places = relationship(
+        "Place",
+        backref='cities',
+        cascade="all, delete",
+        passive_deletes=True)
