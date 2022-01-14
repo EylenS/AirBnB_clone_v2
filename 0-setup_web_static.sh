55badb6609334c9e1ff9087040abcfa43cb58199
eylens <eymasac@gmail.com>
2022-01-13 20:31:53 -0500

    include sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,21 +7,21 @@ sudo apt-get -y install nginx
 sudo service nginx start
 
 # Create folders and file html
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file
-touch /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
 echo "Welcome!" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content 
-sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 
 # Restart Nginx after updating the configuration
-service nginx restart
+sudo service nginx restart
