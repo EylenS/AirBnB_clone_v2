cb20d659ed37e52f173cbd399b7127fbce83bb57
eylens <eymasac@gmail.com>
2022-01-14 16:11:54 -0500

    add last line

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -48,4 +48,5 @@ def deploy():
     archive_path = do_pack()
     if archive_path is None:
         return False
-    return do_deploy(archive_path)
+    else:
+        return do_deploy(archive_path)
