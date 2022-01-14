ebe78e0a14e43af9ec609dacf1331d0e0b52a9ee
eylens <eymasac@gmail.com>
2022-01-14 15:51:30 -0500

    update the file, include previous methods

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,9 +9,39 @@ env.user = 'ubuntu'
 env.hosts = ['34.138.250.25', '50.17.152.241']
 
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+def do_pack():
+    '''generates a .tgz archive from the contents of the web_static folder '''
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = local(
+        "tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+    if file:
+        return ("versions/web_static_{}.tgz web_static".format(date))
+    else:
+        return None
 
+def do_deploy(archive_path):
+    '''This method deployes archive'''
+    if exists(archive_path):
+        dir_ex = archive_path.split('/')[-1]
+        no_tgz = dir_ex.split('.')[0]
+        path = "/data/web_static/releases/"
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # make sure the directory is there
+        run("mkdir -p {}{}/".format(path, no_tgz))
+        # Uncompress the archive to the folder /data/web_static/releases/
+        run("tar -xzf /tmp/{} -C {}{}/".format(dir_ex, path, no_tgz))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(dir_ex))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_tgz))
+        run("rm -rf {}{}/web_static".format(path, no_tgz))
+        run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_tgz))
+        return True
+    else:
+        return False
 
 def deploy():
     '''creates and distributes an archive to your web servers'''
