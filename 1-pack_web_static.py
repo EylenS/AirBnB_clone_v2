561d0ece1c4e57b1abf6cb896a8eefd815ee41aa
eylens <eymasac@gmail.com>
2022-01-13 21:22:56 -0500

    Creating SetEnv.sh

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This module xontents do:pack method'''
+from fabric.api import local
+from datetime import date, datetime
+
+
+def do_pack():
+    '''generates a .tgz archive from the contents of the web_static folder '''
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+    if file:
+        return ("versions/web_static_{}.tgz web_static".format(date))
+    else:
+        return None
