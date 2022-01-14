a2d07d7c6d1d5f5682f0d42db3bcc56e7d015c73
eylens <eymasac@gmail.com>
2022-01-14 10:05:26 -0500

    paths adjusted, and folder with extension .tgz name was changed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,16 +24,20 @@ def do_pack():
 def do_deploy(archive_path):
     '''This method deployes archive'''
     if exists(archive_path):
-        dir = archive_path.split('/')[-1]
-        no_tgz = dir.split('.')[0]
+        dir_ex = archive_path.split('/')[-1]
+        no_tgz = dir_ex.split('.')[0]
         path = '/data/web_static/releases/'
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}{}".format(path, no_tgz))
-        run("tar -xzf /tmp/{} -C {}{}/".format(dir, path, no_tgz))
-        run("rm /tmp/{}".format(dir))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/{}".format(dir_ex))
+        run("mkdir -p {}{}/".format(path, no_tgz))
+        # Uncompress the archive to the folder /data/web_static/releases/
+        run("tar -xzf /tmp/{} -C {}{}/".format(dir_ex, path, no_tgz))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(dir_ex))
         run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_tgz))
         run("rm -rf {}{}/web_static".format(path, no_tgz))
         run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link
         run("ln -s {}{}/ /data/web_static/current".format(path, no_tgz))
         return True
     else:
